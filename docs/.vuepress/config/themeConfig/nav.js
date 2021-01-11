003e436f42f30cd07921f2b89414d96d64a92837
tanyuzhi <admin@yuki520.cn>
2021-01-11 18:58:18 +0800

    fix: 目录

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -48,10 +48,7 @@ module.exports = [
     text: '技术',
     link: '/technology/',
     items: [
-      { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-      { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+      { text: '前端文档', link: '/pages/9a7ee40fc232253e/' }
     ],
   },
   {
