347beb6f54f1430961c418e3dcaa5bf690993e4f
tanyuzhi <admin@yuki520.cn>
2021-01-12 14:27:32 +0800

    fix: 删除部分文档

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -4,37 +4,6 @@ module.exports = [
   {
     text: '前端',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-    items: [
-      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      {
-        text: '前端文章',
-        items: [
-          { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-          { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
-        ],
-      },
-      {
-        text: '学习笔记',
-        items: [
-          { text: '《JavaScript教程》笔记', link: '/note/javascript/' },
-          { text: '《JavaScript高级程序设计》笔记', link: '/note/js/' },
-          { text: '《ES6 教程》笔记', link: '/note/es6/' },
-          { text: '《Vue》笔记', link: '/note/vue/' },
-          {
-            text: '《TypeScript 从零实现 axios》',
-            link: '/note/typescript-axios/',
-          },
-          {
-            text: '《Git》学习笔记',
-            link: '/note/git/',
-          },
-          {
-            text: '《TypeScript》笔记',
-            link: '/pages/51afd6/',
-          },
-        ],
-      },
-    ],
   },
   {
     text: '页面',
@@ -46,19 +15,7 @@ module.exports = [
   },
   {
     text: '文档',
-    link: '/technology/',
-    items: [
-      { 
-        text: '前端文档', 
-        link: '/pages/a18b2c/',
-        items: [
-          {
-            text: 'CSS权威指南第4版', 
-            link: '/pages/a18b2c/'
-          }
-        ]
-      }
-    ],
+    link: '/technology/'
   },
   {
     text: '更多',
